ec980722ee4924c239a94fb9289aa767dde1cf15
Laura Bąkała <Laura.Bakala@ttsi.com.pl>
2023-10-18 13:12:25 +0200

    add retry when connecting

--- tests/testthat/test-randomize-simple.R
+++ tests/testthat/test-randomize-simple.R
@@ -2,6 +2,7 @@ test_that("hello world endpoint returns expected message", {
   response <- request(api_url) |>
     req_url_path("simple", "hello") |>
     req_method("GET") |>
+    req_retry(max_tries = 10) |>
     req_perform() |>
     resp_body_json()
   
