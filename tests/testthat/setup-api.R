39cfe021b3ee166bd64d06c93397b7d62687f3b3
Laura Bąkała <Laura.Bakala@ttsi.com.pl>
2023-10-19 14:47:21 +0200

    setup tests to work locally too

--- tests/testthat/setup-api.R
+++ tests/testthat/setup-api.R
@@ -1,16 +1,21 @@
 api_url <- "http://api:3838"
 
-if (!as.logical(Sys.getenv("CI"))) {
+if (!isTRUE(as.logical(Sys.getenv("CI")))) {
+  withr::local_envvar(
+    # Extract current SHA and set it as a temporary env var
+    list(GITHUB_SHA = system("git rev-parse HEAD", intern = TRUE))
+  )
+  
   # Overwrite API URL if not on CI
-  api_url <- "http://127.0.0.1:5556"
+  api_url <- "http://localhost:3838"
   api_path <- tempdir()
   
   # Start the API
   api <- callr::r_bg(\(path) {
     # 1. Set path to `path`
     # 2. Build a plumber API
-    plumber::plumb(dir = "api") |>
-      plumber::pr_run(port = 5556)
+    plumber::plumb(dir = fs::path("..", "..", "api")) |>
+      plumber::pr_run(port = 3838)
   }, args = list(path = api_path))
   
   # Wait until started
