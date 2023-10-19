407e276dee4a4bcac77ab4af0b698ad0a8220eb6
Laura Bąkała <Laura.Bakala@ttsi.com.pl>
2023-10-19 14:46:50 +0200

    add /meta/sha as endpoint

--- api/plumber.R
+++ api/plumber.R
@@ -1,7 +1,9 @@
 #* @plumber
 function(api) {
   rand_simple <- plumber::pr("randomize-simple.R")
+  meta <- plumber::pr("meta.R")
   
   api |>
-    plumber::pr_mount("/simple", rand_simple)
+    plumber::pr_mount("/simple", rand_simple) |>
+    plumber::pr_mount("/meta", meta)
 }
--- api/randomize-simple.R
+++ api/randomize-simple.R
@@ -1,10 +1,11 @@
 #* Return hello world
 #* 
 #* @get /hello
+#* @serializer unboxedJSON
 function() {
   call_hello_world()
 }
 
 call_hello_world <- function() {
-  Sys.getenv("GITHUB_SHA", unset = "none")
+  "Hello TTSI!"
 }
