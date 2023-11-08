27deaab1db104023ceb0bc02f99b7e520c258ea3
kinga <kinga.salata@ttsi.com.pl>
2023-11-08 14:37:49 +0000

    Added block randomization (randomize-block.R) with helping functions (helpers.R) and changes in plumber.R

--- api/plumber.R
+++ api/plumber.R
@@ -1,9 +1,25 @@
+dirs <- list.dirs(recursive = FALSE)
+if (!any(dirs == "./studies")) {
+  dir.create("studies")
+}
+
+source("helpers.R")
+
+#* @filter Log request
+function(req){
+  print_log(req$REQUEST_METHOD, req$PATH_INFO)
+  plumber::forward()
+}
+
 #* @plumber
+
 function(api) {
   rand_simple <- plumber::pr("randomize-simple.R")
+  rand_block <- plumber::pr("randomize-block.R")
   meta <- plumber::pr("meta.R")
   
   api |>
     plumber::pr_mount("/simple", rand_simple) |>
-    plumber::pr_mount("/meta", meta)
+    plumber::pr_mount("/block", rand_block) |> 
+    plumber::pr_mount("/meta", meta) 
 }
