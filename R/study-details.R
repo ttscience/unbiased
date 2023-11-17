4fb887d33a30f84300a4dfbc8767b4ee7381a591
Laura Bąkała <Laura.Bakala@ttsi.com.pl>
2023-11-17 14:00:29 +0100

    document new functions

--- R/study-details.R
+++ R/study-details.R
@@ -1,16 +1,16 @@
+#' Read study details
+#'
+#' @description
+#' Queries the DB for the study parameters, including declared arms and strata.
+#'
+#' @param study_id `integer(1)`\cr
+#'  ID of the study.
+#'
+#' @return A tibble with study details, containing potentially complex columns,
+#' like `arms`.
+#'
+#' @export
 read_study_details <- function(study_id) {
-  study <- tbl(CONN, "study") |>
-    filter(id == study_id) |>
-    select(id, name, identifier, method_id, parameters) |>
-    left_join(
-      tbl(CONN, "method") |>
-        select(id, method = name),
-      join_by(method_id == id)
-    ) |>
-    select(-method_id) |>
-    collect() |>
-    mutate(parameters = list(jsonlite::fromJSON(parameters)))
-
   arms <- tbl(CONN, "arm") |>
     filter(study_id == study_id) |>
     select(name, ratio) |>
@@ -23,11 +23,21 @@ read_study_details <- function(study_id) {
     mutate(values = list(read_stratum_values(id, value_type)), .by = id) |>
     select(-id)
 
-  mutate(
-    study,
-    arms = list(arms),
-    strata = list(strata)
-  )
+  tbl(CONN, "study") |>
+    filter(id == study_id) |>
+    select(id, name, identifier, method_id, parameters) |>
+    left_join(
+      tbl(CONN, "method") |>
+        select(id, method = name),
+      join_by(method_id == id)
+    ) |>
+    select(-method_id) |>
+    collect() |>
+    mutate(
+      parameters = list(jsonlite::fromJSON(parameters)),
+      arms = list(arms),
+      strata = list(strata)
+    )
 }
 
 read_stratum_values <- function(stratum_id, value_type) {
