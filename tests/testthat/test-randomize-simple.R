1d7c568e3b05cc4ad4cf95f671fcd62bc781a165
Ola <aleksandra.duda@ttsi.com.pl>
2023-12-11 14:55:10 +0000

    correct number of patients

--- tests/testthat/test-randomize-simple.R
+++ tests/testthat/test-randomize-simple.R
@@ -35,7 +35,7 @@ test_that("incorrect parameters raise an exception", {
 })
 
 test_that("proportions are kept (allocation 1:1)", {
-  function_result <- sapply(1:100, function(x) randomize_simple(c("armA", "armB"), c(1,1)))
+  function_result <- sapply(1:1000, function(x) randomize_simple(c("armA", "armB"), c(1,1)))
   x <- prop.test(x = sum(function_result == "armA"), n = length(function_result), p = 0.5, conf.level = 0.95,
                  correct = FALSE)
   y <- prop.test(x = sum(function_result == "armB"), n = length(function_result), p = 0.5, conf.level = 0.95,
@@ -55,8 +55,6 @@ test_that("proportions are kept (allocation 2:1)", {
                  correct = FALSE)
   y <- prop.test(x = sum(function_result == "armB"), n = length(function_result), p = 1/3, conf.level = 0.95,
                  correct = FALSE)
-  x
-  y
   # precision 0.01
   expect_gt(x$p.value, 0.01)
   if (TRUE) {
