26050ed0313c04e39a08fbede2bb6df9b438bcb7
Ola <aleksandra.duda@ttsi.com.pl>
2023-12-11 14:52:01 +0000

    add tests for simple randomization: one-sample prop tests for two and more groups with equal and unequal ratio

--- tests/testthat/test-randomize-simple.R
+++ tests/testthat/test-randomize-simple.R
@@ -33,3 +33,69 @@ test_that("incorrect parameters raise an exception", {
   # Empty arm name
   expect_error(randomize_simple(c("llama", ""), c(2, 3)))
 })
+
+test_that("proportions are kept (allocation 1:1)", {
+  function_result <- sapply(1:100, function(x) randomize_simple(c("armA", "armB"), c(1,1)))
+  x <- prop.test(x = sum(function_result == "armA"), n = length(function_result), p = 0.5, conf.level = 0.95,
+                 correct = FALSE)
+  y <- prop.test(x = sum(function_result == "armB"), n = length(function_result), p = 0.5, conf.level = 0.95,
+                 correct = FALSE)
+
+  # precision 0.01
+  expect_gt(x$p.value, 0.01)
+  if (TRUE) {
+    expect_gt(y$p.value, 0.01)
+  }
+
+})
+
+test_that("proportions are kept (allocation 2:1)", {
+  function_result <- sapply(1:1000, function(x) randomize_simple(c("armA", "armB"), c(2,1)))
+  x <- prop.test(x = sum(function_result == "armA"), n = length(function_result), p = 2/3, conf.level = 0.95,
+                 correct = FALSE)
+  y <- prop.test(x = sum(function_result == "armB"), n = length(function_result), p = 1/3, conf.level = 0.95,
+                 correct = FALSE)
+  x
+  y
+  # precision 0.01
+  expect_gt(x$p.value, 0.01)
+  if (TRUE) {
+    expect_gt(y$p.value, 0.01)
+  }
+})
+
+test_that("proportions are kept (allocation 1:1:1)", {
+  function_result <- sapply(1:1000, function(x) randomize_simple(c("armA", "armB", "armC"), c(1,1,1)))
+  x <- prop.test(x = sum(function_result == "armA"), n = length(function_result), p = 1/3, conf.level = 0.95,
+                 correct = FALSE)
+  y <- prop.test(x = sum(function_result == "armB"), n = length(function_result), p = 1/3, conf.level = 0.95,
+                 correct = FALSE)
+  z <- prop.test(x = sum(function_result == "armC"), n = length(function_result), p = 1/3, conf.level = 0.95,
+                 correct = FALSE)
+  # precision 0.01
+  expect_gt(x$p.value, 0.01)
+  if (TRUE) {
+  expect_gt(y$p.value, 0.01)
+  }
+  if (TRUE) {
+    expect_gt(z$p.value, 0.01)
+  }
+})
+
+test_that("proportions are kept (allocation 1:2:1)", {
+  function_result <- sapply(1:1000, function(x) randomize_simple(c("armA", "armB", "armC"), c(1,2,1)))
+  x <- prop.test(x = sum(function_result == "armA"), n = length(function_result), p = 1/4, conf.level = 0.95,
+                 correct = FALSE)
+  y <- prop.test(x = sum(function_result == "armB"), n = length(function_result), p = 1/2, conf.level = 0.95,
+                 correct = FALSE)
+  z <- prop.test(x = sum(function_result == "armC"), n = length(function_result), p = 1/4, conf.level = 0.95,
+                 correct = FALSE)
+  # precision 0.01
+  expect_gt(x$p.value, 0.01)
+  if (TRUE) {
+    expect_gt(y$p.value, 0.01)
+  }
+  if (TRUE) {
+    expect_gt(z$p.value, 0.01)
+  }
+})
