2e5366a40e2794fc6bffee033b6726e491fdd310
Łukasz Wałejko <lukasz.walejko@ttsi.com.pl>
2024-01-09 07:36:29 +0000

    add autoreload

---
+++ autoreload.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+COMMAND=$1
+
+echo "Running $COMMAND"
+
+watchmedo auto-restart \
+    --patterns="*.R;*.txt" \
+    --ignore-patterns="renv" \
+    --recursive \
+    --directory="./R" \
+    --directory="./inst" \
+    --directory="./tests" \
+    "$@"
